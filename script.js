b3ce6eea6f603bba8393ecad6dfb08d345d36244
unknown <jonafi@gmail.com>
2020-04-15 20:43:18 -0500

    added validation code

--- script.js
+++ script.js
@@ -5,11 +5,7 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
-  
-
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
@@ -17,52 +13,78 @@ generateBtn.addEventListener("click", writePassword);
 
 function generatePassword(){
   //Complete list of valid characters as object containing 4 arrays
-var validCharacters = {
-  "numbers": [0,1,2,3,4,5,6,7,8,9],
-  "lowers": ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],
-  "uppers": ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"],
-  "specials":["!","@","#","$","%","^","&","*","(",")","~","_","+","="]
-};
+  var validCharacters = {
+    "numbers": [0,1,2,3,4,5,6,7,8,9],
+    "lowers": ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],
+    "uppers": ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"],
+    "specials":["!","@","#","$","%","^","&","*","(",")","~","_","+","="]
+  };
 
-//password length, prompt user until valid number entered
-var passwordlength;
+  //password length, prompt user until valid number entered
+  var passwordlength;
 
-if(typeof passwordlength !=="number" ){
-  passwordlength = prompt("Password length \n must be 8-128 characters");
-  passwordlength = parseInt(passwordlength);
-  while (passwordlength<8){
-    passwordlength = prompt("Password length is TOO SMALL.\nPlease enter a value of 8-128");
-  }
-  while(passwordlength>128){
+  if(typeof passwordlength !=="number" ){
+    passwordlength = prompt("Password length \n must be 8-128 characters");
+    passwordlength = parseInt(passwordlength);
+    while (passwordlength<8){
+      passwordlength = prompt("Password length is TOO SMALL.\nPlease enter a value of 8-128");
+    }
+    while(passwordlength>128){
     passwordlength = prompt("Password length is TOO LARGE.\nPlease enter a value of 8-128");
+    }
   }
-}
 
-//random element function, accepts array, picks out a random array element
-function randomChoice(array){
-  var num =Math.floor(Math.random()*array.length);
-  return array[num];
-}
+  //random element function, accepts array, picks out a random array element
+  function randomChoice(array){
+   var num =Math.floor(Math.random()*array.length);
+   return array[num];
+  }
 
-//confirm password preferences
-var userPrefs = {
-  "lowercase" : confirm("Use lowecase letters?"),
-  "uppercase" : confirm("Use uppercase letters?"),
-  "number" : confirm("Use numbers?"),
-  "special" : confirm("Use special characters?")
-}
-var validArrays = [];
-if(userPrefs.lowercase){validArrays.push(validCharacters.lowers);}
-if(userPrefs.uppercase){validArrays.push(validCharacters.uppers);}
-if(userPrefs.number){validArrays.push(validCharacters.numbers);}
-if(userPrefs.special){validArrays.push(validCharacters.specials);}
+  //confirm password preferences
+  var userPrefs = {
+    "lowercase" : confirm("Use lowecase letters?"),
+    "uppercase" : confirm("Use uppercase letters?"),
+    "number" : confirm("Use numbers?"),
+    "special" : confirm("Use special characters?")
+    }
+  var validArrays = [];
+    if(userPrefs.lowercase){validArrays.push(validCharacters.lowers);}
+    if(userPrefs.uppercase){validArrays.push(validCharacters.uppers);}
+    if(userPrefs.number){validArrays.push(validCharacters.numbers);}
+    if(userPrefs.special){validArrays.push(validCharacters.specials);}
 
-//for every character of the password, picks a random valid set, and random character within the set
-var password="";
-for(var i=0;i<passwordlength;i++)
-{
-  var selectedCharacter = randomChoice(randomChoice(validArrays));
-  password = password.concat(selectedCharacter);
-}
+  //for every character of the password, picks a random valid set, and random character within the set
+  var password="";
+    for(var i=0;i<passwordlength;i++){
+      var selectedCharacter = randomChoice(randomChoice(validArrays));
+    password = password.concat(selectedCharacter);
+  }
+
+//This hot garbage section is because I forgot to add the requirement : "THEN my input should be validated and at least one character type should be selected"  
+
+      //console.log("original password: " + password);
+      if((userPrefs.lowercase === true) && (/[a-z]/.test(password) === false)){
+      // console.log('lowercase missing, do something');
+        password = password.concat(randomChoice(validCharacters.lowers));
+      }
+      if((userPrefs.uppercase === true) && (/[A-Z]/.test(password) === false)){
+      // console.log('uppercase missing, do something');
+        password = password.concat(randomChoice(validCharacters.uppers));
+      }
+      if((userPrefs.number === true) && (/[0-9]/.test(password) === false)){
+      // console.log('number missing, do something');
+        password = password.concat(randomChoice(validCharacters.numbers));
+      }
+      if((userPrefs.special === true) && (/[!@#$%^&*()~_+=]/.test(password) === false)){
+        // console.log('special missing, do something');
+        password = password.concat(randomChoice(validCharacters.specials));
+      }
+      while(password.length > passwordlength){
+      password = password.slice(1);
+      }
+//End hot garbage
+
 return password;
 }
+
+

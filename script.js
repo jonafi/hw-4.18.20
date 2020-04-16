a1ccd5a11b57402849e6f1a540f852bfecf50186
unknown <jonafi@gmail.com>
2020-04-15 21:10:49 -0500

    fixed bug in password length loop

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
@@ -26,18 +25,20 @@ function generatePassword(){
   if(typeof passwordlength !=="number" ){
     passwordlength = prompt("Password length \n must be 8-128 characters");
     passwordlength = parseInt(passwordlength);
-    while (passwordlength<8){
+
+  while (passwordlength<8 || passwordlength>128) {
+    if(passwordlength<8){
       passwordlength = prompt("Password length is TOO SMALL.\nPlease enter a value of 8-128");
     }
-    while(passwordlength>128){
-    passwordlength = prompt("Password length is TOO LARGE.\nPlease enter a value of 8-128");
+    if(passwordlength>128){
+     passwordlength = prompt("Password length is TOO LARGE.\nPlease enter a value of 8-128");
     }
   }
 
   //random element function, accepts array, picks out a random array element
   function randomChoice(array){
-   var num =Math.floor(Math.random()*array.length);
-   return array[num];
+    var num =Math.floor(Math.random()*array.length);
+    return array[num];
   }
 
   //confirm password preferences
@@ -46,7 +47,7 @@ function generatePassword(){
     "uppercase" : confirm("Use uppercase letters?"),
     "number" : confirm("Use numbers?"),
     "special" : confirm("Use special characters?")
-    }
+  }
   var validArrays = [];
     if(userPrefs.lowercase){validArrays.push(validCharacters.lowers);}
     if(userPrefs.uppercase){validArrays.push(validCharacters.uppers);}
@@ -55,36 +56,35 @@ function generatePassword(){
 
   //for every character of the password, picks a random valid set, and random character within the set
   var password="";
-    for(var i=0;i<passwordlength;i++){
-      var selectedCharacter = randomChoice(randomChoice(validArrays));
+  for(var i=0;i<passwordlength;i++){
+    var selectedCharacter = randomChoice(randomChoice(validArrays));
     password = password.concat(selectedCharacter);
   }
 
-//This hot garbage section is because I forgot to add the requirement : "THEN my input should be validated and at least one character type should be selected"  
+  //I apologize for the following validation hot garbage 
 
-      //console.log("original password: " + password);
-      if((userPrefs.lowercase === true) && (/[a-z]/.test(password) === false)){
-      // console.log('lowercase missing, do something');
-        password = password.concat(randomChoice(validCharacters.lowers));
-      }
-      if((userPrefs.uppercase === true) && (/[A-Z]/.test(password) === false)){
-      // console.log('uppercase missing, do something');
-        password = password.concat(randomChoice(validCharacters.uppers));
-      }
-      if((userPrefs.number === true) && (/[0-9]/.test(password) === false)){
-      // console.log('number missing, do something');
-        password = password.concat(randomChoice(validCharacters.numbers));
-      }
-      if((userPrefs.special === true) && (/[!@#$%^&*()~_+=]/.test(password) === false)){
-        // console.log('special missing, do something');
-        password = password.concat(randomChoice(validCharacters.specials));
-      }
-      while(password.length > passwordlength){
-      password = password.slice(1);
-      }
-//End hot garbage
+  //console.log("original password: " + password);
+  if((userPrefs.lowercase === true) && (/[a-z]/.test(password) === false)){
+    // console.log('lowercase missing, do something');
+    password = password.concat(randomChoice(validCharacters.lowers));
+  }
+  if((userPrefs.uppercase === true) && (/[A-Z]/.test(password) === false)){
+    // console.log('uppercase missing, do something');
+    password = password.concat(randomChoice(validCharacters.uppers));
+  }
+  if((userPrefs.number === true) && (/[0-9]/.test(password) === false)){
+    // console.log('number missing, do something');
+    password = password.concat(randomChoice(validCharacters.numbers));
+  }
+  if((userPrefs.special === true) && (/[!@#$%^&*()~_+=]/.test(password) === false)){
+    // console.log('special missing, do something');
+    password = password.concat(randomChoice(validCharacters.specials));
+  }
+  while(password.length > passwordlength){
+    password = password.slice(1);
+  }
+  //End hot garbage
 
-return password;
+  return password;
+  }
 }
-
-
